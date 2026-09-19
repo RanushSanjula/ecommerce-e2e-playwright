cbb4798496f1f80e3b4db19d78c9de8898b7db01
RanushSanjula <ranush1999san@gmail.com>
2026-09-19 22:53:04 +0530

    Updated checkout page

--- tests/checkout.spec.js
+++ tests/checkout.spec.js
@@ -2,19 +2,33 @@ import { test, expect } from '@playwright/test';
 import { createSigninData } from '../data/signin.data';
 import { signinPage } from '../pages/signin';
 import { checkoutPage } from '../pages/checkout';
+import { cartPage } from '../pages/cart';
 
 let newSignin;
 let newCheckout;
+let newCart;
 
 test.beforeEach(async ({ page }) => {
 
     newSignin = new signinPage(page);
+    newCart = new cartPage(page);
     await newSignin.navigate();
     await newSignin.signin(createSigninData());
     await expect.poll(async () => (await page.locator('#nameofuser').textContent())?.trim(), {
         timeout: 15000,
     })
         .toBe(`Welcome ${createSigninData().username}`);
+    await newCart.cart();
+
+    let alertMessage = '';
+
+    page.once('dialog', async (dialog) => {
+        alertMessage = dialog.message();
+        await dialog.accept();
+    });
+
+    await expect.poll(() => alertMessage).toBe('Product added.');
+    await page.locator('#navbarExample > ul > li.nav-item.active > a').click();
 
 })
 
