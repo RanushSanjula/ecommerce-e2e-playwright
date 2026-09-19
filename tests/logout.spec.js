c498e75dca2e9b16bb0b511663ff78c82a2386c3
RanushSanjula <ranush1999san@gmail.com>
2026-09-19 17:56:19 +0530

    Created POM for log out

--- tests/logout.spec.js
+++ tests/logout.spec.js
@@ -1,23 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { createSigninData } from '../data/signin.data';
+import { signinPage } from '../pages/signin'
 
+let newSignin;
 
 test.beforeEach(async ({ page }) => {
 
-    await page.goto('https://www.demoblaze.com/');
+    newSignin = new signinPage(page);
+    await newSignin.navigate();
 
 })
 
 test('Successful logout(P)', async ({ page }) => {
 
-    await page.locator('id=login2').click();
-    await page.locator('id=loginusername').fill(createSigninData().username);
-    await page.locator('id=loginpassword').fill(createSigninData().password);
-    await page.getByRole('button', { name: 'Log in' }).click();
-    await expect.poll(async () => (await page.locator('#nameofuser').textContent())?.trim(), {
-        timeout: 15000,
-    })
-        .toBe(`Welcome ${createSigninData().username}`);
+    await newSignin.signin(createSigninData());
     await page.locator('id=logout2').click();
     await expect(page.locator('id=login2')).toBeVisible();
 
